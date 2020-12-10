b4c02d63e89f685b8a23386b6eafe3563e432869
ghddbwjd20 <ghddbwjd20@naver.com>
2020-12-11 01:00:25 +0900

    goroutine

---
+++ go_goroutine_01.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time" //java Thread.sleep(5000)
+)
+
+func x(){
+	for i:=0 ; i<10 ; i ++{
+		fmt.Print("x")
+}
+}
+
+func y(){
+        for i:=0 ; i<10 ; i ++{
+                fmt.Print("y")
+}
+}
+func z(){
+	fmt.Println("z")
+}
+
+func k(){
+	fmt.Println("k")
+}
+func main(){
+	defer z()
+	defer k() //fifo
+	go x()
+	go y()
+	time.Sleep((time.Second *2)
+	fmt.Println("end main()")
+}
